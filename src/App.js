c682554538b6f0f90bca6067cdf436d45d9a29b1
nuka <boragu1234@gmail.com>
2024-09-30 00:29:19 +0900

    커밋테스트

--- src/App.js
+++ src/App.js
@@ -59,14 +59,14 @@ function App() {
 }
 
 
-function Modal(props){
-  return(
-    <div className='modal'>
-      <h4>{props.props글제목[props.title]}</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
-  )
-}
+// function Modal(props){
+//   return(
+//     <div className='modal'>
+//       <h4>{props.props글제목[props.title]}</h4>
+//       <p>날짜</p>
+//       <p>상세내용</p>
+//     </div>
+//   )
+// }
 
 export default App;
